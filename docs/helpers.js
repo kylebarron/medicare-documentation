b3f5a803a894795e763ce2261cecd2b9e8998cc5
Kyle Barron <kylebarron2@gmail.com>
2017-12-20 12:47:34 -0500

    Update

--- docs/helpers.js
+++ docs/helpers.js
@@ -1,3 +1,24 @@
+// window.MathJax = {
+//   tex2jax: {
+//     inlineMath: [ ["\\(","\\)"] ],
+//     displayMath: [ ['$$','$$'] ]
+//   },
+//   TeX: {
+//     TagSide: "right",
+//     TagIndent: ".8em",
+//     MultLineWidth: "85%",
+//     equationNumbers: {
+//       autoNumber: "AMS",
+//     },
+//     unicode: {
+//       fonts: "STIXGeneral,'Arial Unicode MS'"
+//     }
+//   },
+//   displayAlign: "left",
+//   showProcessingMessages: false,
+//   messageStyle: "none"
+// };
+
 MathJax.Hub.Config({
   TeX: {
     equationNumbers: {
