6883990b38c7f52c86ecfa76db7b3bb5a6ebcb73
Kyle Barron <kylebarron2@gmail.com>
2018-01-16 16:07:42 -0500

    Apply yapf

--- resdac-scrape.py
+++ resdac-scrape.py
@@ -8,15 +8,25 @@ import re
 from time import sleep
 
 urls_dict = {
-    'Beneficiary Summary File': 'https://www.resdac.org/cms-data/files/mbsf/data-documentation',
-    'Carrier RIF': 'https://www.resdac.org/cms-data/files/carrier-rif/data-documentation',
-    'Durable Medical Equipment RIF': 'https://www.resdac.org/cms-data/files/dme-rif/data-documentation',
-    'Home Health Agency RIF': 'https://www.resdac.org/cms-data/files/hha-rif/data-documentation',
-    'Hospice RIF': 'https://www.resdac.org/cms-data/files/hospice-rif/data-documentation',
-    'Inpatient RIF': 'https://www.resdac.org/cms-data/files/ip-rif/data-documentation',
-    'MedPAR RIF': 'https://www.resdac.org/cms-data/files/medpar-rif/data-documentation',
-    'Outpatient RIF': 'https://www.resdac.org/cms-data/files/op-rif/data-documentation',
-    'Skilled Nursing Facility RIF': 'https://www.resdac.org/cms-data/files/snf-rif/data-documentation'}
+    'Beneficiary Summary File':
+        'https://www.resdac.org/cms-data/files/mbsf/data-documentation',
+    'Carrier RIF':
+        'https://www.resdac.org/cms-data/files/carrier-rif/data-documentation',
+    'Durable Medical Equipment RIF':
+        'https://www.resdac.org/cms-data/files/dme-rif/data-documentation',
+    'Home Health Agency RIF':
+        'https://www.resdac.org/cms-data/files/hha-rif/data-documentation',
+    'Hospice RIF':
+        'https://www.resdac.org/cms-data/files/hospice-rif/data-documentation',
+    'Inpatient RIF':
+        'https://www.resdac.org/cms-data/files/ip-rif/data-documentation',
+    'MedPAR RIF':
+        'https://www.resdac.org/cms-data/files/medpar-rif/data-documentation',
+    'Outpatient RIF':
+        'https://www.resdac.org/cms-data/files/op-rif/data-documentation',
+    'Skilled Nursing Facility RIF':
+        'https://www.resdac.org/cms-data/files/snf-rif/data-documentation'
+}
 
 local_paths_dict = {
     'Master Beneficiary Summary File': 'mbsf.md',
@@ -27,8 +37,8 @@ local_paths_dict = {
     'Inpatient RIF': 'ip-rif.md',
     'MedPAR RIF': 'medpar-rif.md',
     'Outpatient RIF': 'op-rif.md',
-    'Skilled Nursing Facility RIF': 'snf-rif.md'}
-
+    'Skilled Nursing Facility RIF': 'snf-rif.md'
+}
 
 all_links = []
 for page_title, url in urls_dict.items():
@@ -38,13 +48,13 @@ for page_title, url in urls_dict.items():
     links = LH.fromstring(page.content).xpath('//tr/td/a/@href')
     all_links.extend(links)
     dfs = pd.read_html(page.content)
-    
+
     # Create local urls from title of pages
     hrefs = []
     for i in range(len(links)):
         def_page = requests.get('http://www.resdac.org' + links[i])
         def_soup = BeautifulSoup(def_page.content, 'html.parser')
-        href = def_soup.find(id = 'page-title').get_text()
+        href = def_soup.find(id='page-title').get_text()
         href = re.sub(r'[.,/#!$%\^&\*;:{}=_`~()]', '', href)
         href = href.strip()
         href = href.lower()
@@ -60,31 +70,37 @@ for page_title, url in urls_dict.items():
     all_tables = []
     for i in range(len(dfs)):
         df = dfs[i]
-        
+
         # Remove phantom 6th column:
-        df = df.iloc[:,:5]
-        
+        df = df.iloc[:, :5]
+
         # Add links
         df['refname'] = hrefs[:len(df)]
         hrefs = hrefs[len(df):]
-        
+
         # Replace NaN with blank string
         df = df.fillna('')
-        
+
         # Make Variable Name column a Markdown link
-        df.iloc[:, 2] = '[' + df.iloc[:, 2] + '](variables.md#' + df['refname'] + ')'
+        df.iloc[:, 2] = (
+            '[' + df.iloc[:, 2] + '](variables.md#' + df['refname'] + ')')
         df = df.drop(columns=['refname'])
-        
+
         # Make Short SAS Name code style
         df.iloc[:, 1] = '`' + df.iloc[:, 1] + '`'
-        
+
         # Write to file
-        text = tabulate(df, headers = ['Index', 'SAS Name', 'Variable Name', 'Limitation', 'Code Table'], tablefmt = 'pipe', showindex = False)
-        
+        text = tabulate(
+            df,
+            headers=[
+                'Index', 'SAS Name', 'Variable Name', 'Limitation', 'Code Table'
+            ],
+            tablefmt='pipe',
+            showindex=False)
+
         all_tables.append('\n### {}\n\n'.format(table_titles[i]))
         all_tables.append(text)
         all_tables.append('\n')
-
 
     # Read in Markdown file
     fname = re.search(r'/([\w-]+)/data-documentation', url)[1]
@@ -107,14 +123,12 @@ for page_title, url in urls_dict.items():
     mdfile.writelines(lines)
     mdfile.close()
 
-
 all_links = sorted(list(set(all_links)))
 all_links = ['http://www.resdac.org' + x for x in all_links]
 
-
 all_text = []
 all_text.append('# Variable Definitions\n\n')
-source  = '!!! note\n    '
+source = '!!! note\n    '
 source += 'These definitions are scraped from ResDAC. Click on '
 source += 'the header of a variable description to see the ResDAC page.\n\n'
 all_text.append(source)
@@ -124,20 +138,23 @@ for url in all_links:
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    section = soup.find(id = 'page-title').get_text()
+    section = soup.find(id='page-title').get_text()
     section = '\n\n## [' + section + '](' + url + ')\n\n'
-    # NOTE! If I use the actual page title, I can't assume the header anchors will use the text in the tables in the documentation. I.e. in the first step, I'd need to get the links that each variable links to, grab the title off of that page, and then generate what the anchor would be.
 
-    short_name = soup.find(class_='field-name-field-short-sas-name').find(class_='field-item').get_text()
-    varnames =  '- Short SAS Name: `{}`\n'.format(short_name.lower())
+    short_name = soup.find(class_='field-name-field-short-sas-name').find(
+        class_='field-item').get_text()
+    varnames = '- Short SAS Name: `{}`\n'.format(short_name.lower())
     try:
-        long_name = soup.find(class_='field-name-field-long-sas-name').find(class_='field-item').get_text()
+        long_name = soup.find(class_='field-name-field-long-sas-name').find(
+            class_='field-item').get_text()
         varnames += '- Long SAS Name: `{}`\n'.format(long_name.lower())
     except:
         pass
     varnames += '\n'
 
-    in_files = [x.get_text() for x in soup.find(class_='view-content').find_all('a')]
+    in_files = [
+        x.get_text() for x in soup.find(class_='view-content').find_all('a')
+    ]
     in_files = [x for x in in_files if x in local_paths_dict.keys()]
 
     contained_in = 'Contained in\n\n'
@@ -146,7 +163,7 @@ for url in all_links:
         contained_in += '- [{}]({}#data-documentation)\n'.format(fname, lpath)
     contained_in += '\n'
 
-    main_text = soup.find_all(id = ['block-system-main', 'region-content'])
+    main_text = soup.find_all(id=['block-system-main', 'region-content'])
     p_text = main_text[0].find_all('p')
     len(p_text)
     text_list = []
@@ -179,7 +196,7 @@ for url in all_links:
 
     # Values
     values_text = ''
-    values_box = soup.find(class_ = 'field-collection-view-final')
+    values_box = soup.find(class_='field-collection-view-final')
     if values_box is not None:
         if not values_box.find('tr'):
             values_text = '### Values\n\n'
@@ -187,18 +204,26 @@ for url in all_links:
             has_tables = False
         else:
             has_tables = True
-            table_headers = [x.get_text() for x in soup.find_all(class_ = 'field-name-field-note')]
+            table_headers = [
+                x.get_text()
+                for x in soup.find_all(class_='field-name-field-note')
+            ]
             tables = pd.read_html(page.content)
-            
+
             all_text_tables = []
             for i in range(len(tables)):
                 try:
                     all_text_tables.append(table_headers[i])
                 except:
                     pass
-                text_table = tabulate(tables[i], headers=['Code', 'Code Value'], tablefmt='pipe', numalign='left', showindex=False)
+                text_table = tabulate(
+                    tables[i],
+                    headers=['Code', 'Code Value'],
+                    tablefmt='pipe',
+                    numalign='left',
+                    showindex=False)
                 all_text_tables.append(text_table)
-            
+
             all_text_tables = '\n\n'.join(all_text_tables)
             values_text = '### Values\n\n'
             values_text += all_text_tables
@@ -212,7 +237,3 @@ for url in all_links:
 mdfile = open(os.path.join('docs', 'resdac', 'variables.md'), 'w')
 mdfile.writelines(all_text)
 mdfile.close()
-
-
-
-
