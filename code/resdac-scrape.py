71ba848a7fe492d78101ddc78987920974934dc9
Kyle Barron <barronk@mit.edu>
2018-01-19 12:18:06 -0500

    Use local dataset for writing text to markdown files

--- code/resdac-scrape.py
+++ code/resdac-scrape.py
@@ -229,8 +229,11 @@ for f in files:
         }])
     df = df.append(row)
 
+df = df.sort_values(by = ['var_title'])
 path = os.path.join('..', 'data', 'variable_info.pkl')
 df.to_pickle(path)
+
+df = pd.read_pickle(path)
 
 ## Create Variable Definitions page
 all_text = []
@@ -240,107 +243,44 @@ source += 'These definitions are scraped from ResDAC. Click on '
 source += 'the header of a variable description to see the ResDAC page.\n\n'
 all_text.append(source)
 
-for url in all_links:
-    sleep(10)
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+i = 0
+for i in range(len(df)):
+    row = df.iloc[i]
 
-    section = soup.find(id='page-title').get_text()
-    section = '\n\n## [' + section + '](' + url + ')\n\n'
-
-    short_name = soup.find(class_='field-name-field-short-sas-name').find(
-        class_='field-item').get_text()
-    varnames = '- Short SAS Name: `{}`\n'.format(short_name.lower())
-    try:
-        long_name = soup.find(class_='field-name-field-long-sas-name').find(
-            class_='field-item').get_text()
-        varnames += '- Long SAS Name: `{}`\n'.format(long_name.lower())
-    except:
-        pass
+    var_title = '\n\n## [{}]({})\n\n'.format(row['var_title'], row['resdac_url'])
+    varnames = '- Short SAS Name: `{}`\n'.format(row['short_sas_name'])
+    if row['long_sas_name'] != '':
+        varnames += '- Long SAS Name: `{}`\n'.format(row['long_sas_name'])
     varnames += '\n'
 
-    in_files = [
-        x.get_text() for x in soup.find(class_='view-content').find_all('a')
-    ]
-    in_files = [x for x in in_files if x in local_paths_dict.keys()]
-
     contained_in = 'Contained in\n\n'
-    for fname in in_files:
+    for fname in row['in_files']:
         lpath = local_paths_dict[fname]
         contained_in += '- [{}]({}#data-documentation)\n'.format(fname, lpath)
     contained_in += '\n'
 
-    main_text = soup.find_all(id=['block-system-main', 'region-content'])
-    p_text = main_text[0].find_all('p')
-    len(p_text)
-    text_list = []
-    for i in range(len(p_text) - 1):
-        text_list.append(p_text[i].get_text())
-
-    text = '\n\n'.join(text_list)
+    text = row['main_text']
     # Format as inline code any text within '' that has at least one digit
     text = re.sub(r"'(\w*\d\w*)'", r'`\1`', text)
     text += '\n\n'
 
     hidden_text = []
-    try:
-        derivation = soup.select('.field-name-field-derivation .even')
-        derivation = [x.get_text() for x in derivation]
-        derivation = '\n\n'.join(derivation)
-        derivation = '??? derivation\n    ' + derivation
-        hidden_text.extend(derivation)
-    except:
-        pass
-
-    try:
-        limitation = soup.select('.field-label-hidden .even')
-        limitation = [x.get_text() for x in limitation]
-        limitation = '\n\n'.join(limitation)
-        limitation = '??? limitation\n    ' + limitation
-        hidden_text.extend(limitation)
-    except:
-        pass
-
-    # Values
-    values_text = ''
-    values_box = soup.find(class_='field-collection-view-final')
-    if values_box is not None:
-        if not values_box.find('tr'):
-            values_text = '### Values\n\n'
-            values_text += values_box.get_text()
-            has_tables = False
-        else:
-            has_tables = True
-            table_headers = [
-                x.get_text()
-                for x in soup.find_all(class_='field-name-field-note')
-            ]
-            tables = pd.read_html(page.content)
-
-            all_text_tables = []
-            for i in range(len(tables)):
-                try:
-                    all_text_tables.append(table_headers[i])
-                except:
-                    pass
-                text_table = tabulate(
-                    tables[i],
-                    headers=['Code', 'Code Value'],
-                    tablefmt='pipe',
-                    numalign='left',
-                    showindex=False)
-                all_text_tables.append(text_table)
-
-            all_text_tables = '\n\n'.join(all_text_tables)
-            values_text = '### Values\n\n'
-            values_text += all_text_tables
-
-    all_text.append(section)
+    if row['derivation'] != '':
+        derivation = '??? derivation\n\t' + row['derivation']
+        hidden_text.append(derivation)
+    if row['limitation'] != '':
+        limitation = '??? limitation\n\t' + row['limitation']
+        hidden_text.append(limitation)
+    hidden_text = '\n\n'.join(hidden_text)
+    
+    
+    
+    all_text.append(var_title)
     all_text.append(varnames)
     all_text.append(contained_in)
     all_text.append(text)
-    all_text.append(values_text)
+    all_text.append(hidden_text)
+    all_text.append(row['values_text'])
 
-mdfile = open(os.path.join('..', 'docs', 'resdac', 'variables.md'), 'w')
-mdfile.writelines(all_text)
-mdfile.close()
+with open(os.path.join('..', 'docs', 'resdac', 'variables.md'), 'w') as mdfile:
+    mdfile.writelines(all_text)
