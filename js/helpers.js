fcb5c0eb0d3e32593544d81531a114ba99a425d7
Kyle Barron <kylebarron2@gmail.com>
2018-02-06 15:01:58 -0500

    Deployed bd70ee6 with MkDocs version: 0.17.2

---
+++ js/helpers.js
@@ -0,0 +1,33 @@
+// window.MathJax = {
+//   tex2jax: {
+//     inlineMath: [ ["\\(","\\)"] ],
+//     displayMath: [ ['$$','$$'] ]
+//   },
+//   TeX: {
+//     TagSide: "right",
+//     TagIndent: ".8em",
+//     MultLineWidth: "85%",
+//     equationNumbers: {
+//       autoNumber: "AMS",
+//     },
+//     unicode: {
+//       fonts: "STIXGeneral,'Arial Unicode MS'"
+//     }
+//   },
+//   displayAlign: "left",
+//   showProcessingMessages: false,
+//   messageStyle: "none"
+// };
+
+MathJax.Hub.Config({
+  TeX: {
+    equationNumbers: {
+      autoNumber: "AMS"
+    }
+  },
+  tex2jax: {
+    inlineMath: [ ['$','$'], ['\\(', '\\)'] ],
+    displayMath: [ ['$$','$$'] ],
+    processEscapes: true,
+  }
+});
